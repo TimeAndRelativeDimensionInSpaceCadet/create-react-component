b1fdefe89e878d6c4eff1ca0498b84315f40bafb
Erik White <ewhite@nvisia.com>
2023-06-07 08:04:52 -0500

    multile args working, prompts occur when required args aren't completed

--- src/cli.js
+++ src/cli.js
@@ -20,36 +20,85 @@ const parseArgumentsIntoOptions = (rawArgs) => {
   );
 
   return {
-    componentType: args[componentTypeFlag],
-    styleType: args[styleFileTypeFlag],
+    componentType:
+      args[componentTypeFlag] && args[componentTypeFlag].toLowerCase(),
+    styleType: args[styleFileTypeFlag] && args[styleFileTypeFlag].toLowerCase(),
     componentName: args._[0],
     directory: args._[1],
   };
 };
 
 const promptForMissingOptions = async (options) => {
-  const questions = [];
-  if (!options.componentName) {
-    questions.push({
+  const validComponentTypes = ["functional", "class"];
+  const validStyleTypes = ["css", "sass"];
+  //const defaultComponentDir = "./src/components";
+  const potentialQuestions = [];
+
+  if (
+    !options.componentName ||
+    (options.componentName && options.componentName.length < 1)
+  ) {
+    potentialQuestions.push({
       type: "string",
       name: "componentName",
-      message: "Please enter a component name",
+      message: "Please enter a component name?",
       default: "",
     });
   }
 
-  const answers = await inquirer.prompt(questions);
+  if (
+    !options.componentType ||
+    !isValidOption(options.componentType, validComponentTypes)
+  ) {
+    potentialQuestions.push({
+      type: "list",
+      name: "componentType",
+      message: "Which type of component would you like to create?",
+      choices: validComponentTypes,
+      default: validComponentTypes[0],
+    });
+  }
 
-  return {
+  if (
+    !options.styleType ||
+    !isValidOption(options.styleType, validStyleTypes)
+  ) {
+    potentialQuestions.push({
+      type: "list",
+      name: "styleType",
+      message: "Which format would you like the stylesheet to use?",
+      choices: validStyleTypes,
+    });
+  }
+
+  /* if (!options.directory || !isValidDirectory(options.directory)) {
+    potentialQuestions.push({
+      type: "string",
+      name: "directory",
+      message: "Which directory should the component be generatede in?",
+      default: defaultComponentDir,
+    });
+  } */
+
+  return await inquirer.prompt(potentialQuestions).then((answers) => ({
     ...options,
     ...answers,
-  };
+  }));
 };
 
-const validateOptions = (options) => {};
+const isValidOption = (inputValue, optionsArr) => {
+  return inputValue && optionsArr.includes(inputValue.toLowerCase());
+};
+
+/* const isValidDirectory = (directory) => {
+  
+}; */
+
+const generateComponent = async () => {};
 
 export const cli = async (args) => {
   const options = parseArgumentsIntoOptions(args);
   const optionsAfterPrompt = await promptForMissingOptions(options);
+
   console.log(optionsAfterPrompt);
 };
